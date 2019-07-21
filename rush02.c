ada1ce2790756338d48713ecb83d76437eebcbfa
Francois LEFEBVRE <frlefebv@e3r6p8.42.fr>
2019-07-21 15:42:08 +0200

    poulet basquaise

---
+++ rush02.c
@@ -0,0 +1,127 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush02.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frlefebv <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/21 01:12:03 by frlefebv          #+#    #+#             */
+/*   Updated: 2019/07/21 03:23:30 by frlefebv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int		check_sep(char c, char *charset)
+{
+		int	j;
+
+		j = 0;
+		while (charset[j])
+		{
+			if (c == charset[j])
+				return (1);
+			++j;
+		}
+		return (0);
+}
+
+int		check_word(char c, char cbefore, char *charset)
+{
+	return (!check_sep(c, charset) && check_sep(cbefore, charset));
+}
+
+int		get_words_count(char *str, char *charset)
+{
+	int	nb_words;
+	int	i;
+
+	i = 0;
+	nb_words = 0;
+	while (str[i] != '\0')
+	{
+		if (check_word(str[i], str[i - 1], charset) ||
+			   	(!check_sep(str[i], charset) && i == 0))
+			nb_words++;
+			i++;
+	}
+	return (nb_words);
+}
+
+int *words_len(char *str, char *charset)
+{
+	int count;
+	int i;
+	int nb_words;
+	int *words_size;
+
+	i = 0;
+	nb_words = get_words_count(str, charset);
+	words_size = malloc(nb_words * sizeof(int));
+	while (i <= nb_words)
+	{
+		words_size[i] = 0;
+		i++;
+	}
+	i = 0;
+	count = 0;
+	while (str[i] != '\0')
+	{
+		if (!check_sep(str[i], charset))
+			words_size[count]++;
+		else if (i > 0 && !check_sep(str[i - 1], charset))
+			count++;
+		i++;
+	}
+	return (words_size);
+}
+
+char **ft_split(char *str, char *charset)
+{
+	char **arr;
+	int k;
+	int j;
+	int count;
+	int *words_size;
+
+	arr = malloc((get_words_count(str, charset) + 1) * sizeof(char*));
+	words_size = words_len(str, charset);
+	count = 0;
+	j = 0;
+	k = -1;
+	while (str[++k] != '\0')
+	{
+		if (!check_sep(str[k], charset))
+		{	
+			if (k == 0 || check_world(str[k], str[k - 1], charset))
+			{
+				arr[count] = malloc(words_size[count] * sizeof(char));
+				arr[count][j] = str[k];
+				arr[count][++j] = '\0';
+			}
+		}	else if (k > 0 && !check_sep(str[k - 1], charset) && ++count)
+			j = 0;
+	}
+	arr[get_word_count(str, charset)] = 0;
+	return (arr);
+}
+
+int main()
+{
+	char *str;
+	char *charset;
+	int i;
+	char **arr;
+
+	i = 0;
+	str = "zero une due tre quattro cinque sei sette otto nove dieci";
+	charset = " ";
+	arr = ft_split(str, charset);
+	while(arr[i])
+	{
+		printf("%s\n", arr[i]);
+		i++;	
+	}
+	return (0);
+}
